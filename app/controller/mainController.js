a3678173a6e747e524807d9ed5ba643e87daf7a9
Dylan GAVAUD <dylan.gavaud.dev@gmail.com>
2024-06-04 16:04:01 +0200

    catalog ejs ajouté

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -2,5 +2,8 @@ const mainController = {
   homePage: (req, res) => {
     res.render("pages/home");
   },
+  catalogPage: (req, res) => {
+    res.render("pages/catalog");
+  },
 };
 module.exports = mainController;
--- app/router.js
+++ app/router.js
@@ -4,5 +4,6 @@ const mainController = require("./controller/mainController");
 const router = express.Router();
 
 router.get("/", mainController.homePage);
+router.get("/catalog", mainController.catalogPage);
 
 module.exports = router;
