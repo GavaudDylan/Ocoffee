62984319b1a589abf6a9401ab09bc8816079541f
Dylan GAVAUD <dylan.gavaud.dev@gmail.com>
2024-06-06 20:34:35 +0200

    boucle forEach ok

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -1,12 +1,51 @@
+const dataMapper = require("../../data/dataMapper");
+
 const mainController = {
-  homePage: (req, res) => {
-    res.render("pages/home");
+  async homePage(req, res) {
+    try {
+      const coffees = await dataMapper.getAllCoffees();
+      res.render("pages/home", {
+        coffees,
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send(`An error occured with the database :\n${error.message}`);
+    }
   },
-  catalogPage: (req, res) => {
-    res.render("pages/catalog");
+
+  async catalogPage(req, res) {
+    try {
+      const coffees = await dataMapper.getAllCoffees();
+      res.render("pages/catalog", {
+        coffees,
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send(`An error occured with the database :\n${error.message}`);
+    }
   },
-  shopPage: (req, res) => {
-    res.render("pages/shop");
+  async shopPage(req, res, next) {
+    try {
+      const espressoId = 1;
+      const espresso = await dataMapper.getCoffeById(espressoId);
+      console.log(espresso);
+      if (!espresso) {
+        next;
+        return;
+      }
+      res.render("pages/shop", {
+        espresso,
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send(`An error occured with the database :\n${error.message}`);
+    }
   },
 };
 module.exports = mainController;
--- data/dataMapper.js
+++ data/dataMapper.js
@@ -2,9 +2,18 @@ const client = require("./database");
 
 const dataMapper = {
   async getAllCoffees() {
-    const query = `SELECT * FROM coffee`;
+    const query = `SELECT * FROM "coffee"`;
     const result = await client.query(query);
+    console.log(result.rows);
     return result.rows;
+  },
+
+  async getCoffeById(coffeeId) {
+    const result = await client.query(`SELECT * FROM coffee WHERE id = $1`, [
+      coffeeId,
+    ]);
+    const coffeeInfo = result.rows[0];
+    return coffeeInfo || null;
   },
 };
 
