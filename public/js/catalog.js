d814609a03e0a92cfeb6f1d6e3b697f9e7ef3c1b
Dylan GAVAUD <dylan.gavaud.dev@gmail.com>
2024-06-07 16:52:43 +0200

    Afficher tout les articles ok

---
+++ public/js/catalog.js
@@ -0,0 +1,9 @@
+const viewAllButton = document.getElementById("viewAllButton");
+const hiddenArticles = document.querySelectorAll(".detail-container.hidden");
+
+viewAllButton.addEventListener("click", () => {
+  hiddenArticles.forEach((article) => {
+    article.classList.remove("hidden");
+  });
+  viewAllButton.style.display = "none";
+});
