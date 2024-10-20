72323aca255c0bf4975fc35a92d0cbcbda6a2175
GavaudDylan <dylan.gavaud.dev@gmail.com>
2024-10-20 19:39:53 +0200

    add repo on my workspace

--- public/js/catalog.js
+++ public/js/catalog.js
@@ -2,8 +2,8 @@ const viewAllButton = document.getElementById("viewAllButton");
 const hiddenArticles = document.querySelectorAll(".detail-container.hidden");
 
 viewAllButton.addEventListener("click", () => {
-  hiddenArticles.forEach((article) => {
-    article.classList.remove("hidden");
-  });
-  viewAllButton.style.display = "none";
+	hiddenArticles.forEach((article) => {
+		article.classList.remove("hidden");
+	});
+	viewAllButton.style.display = "none";
 });
